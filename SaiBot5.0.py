550107180c13574c7756a8229e276dd1f7fc792a
unknown <noah.v.olson@gmail.com>
2020-07-16 16:34:11 -0400

    centralized all scene drawing into Map class

--- SaiBot5.0.py
+++ SaiBot5.0.py
@@ -6,12 +6,20 @@ from MapProcessor import *
 from Astar import *
 
 
-class GraphicsScene(qtw.QGraphicsScene):
+class Map(qtw.QGraphicsScene):
     def __init__(self, parent=None):
         qtw.QGraphicsScene.__init__(self, parent)
 
-        # for redraw and destination storage
+        # only generate walkable data if it doesn't exist
+        try:
+            self.mapData = np.loadtxt('mapData.csv', delimiter=',')
+        except:
+            self.mapData = initWalkable()
+            np.savetxt('mapData.csv', mapData, delimiter=',')
+        
         self.selected = []
+        self.walkToggle = False
+        self.grassToggle = False
         
     def mousePressEvent(self, event):
 
@@ -24,14 +32,53 @@ class GraphicsScene(qtw.QGraphicsScene):
         self.selected.append((x, y))
         self.addRect(x,y,10,10,yellowPen)
 
-    def redrawSelected(self):
+    def update(self):
+        self.clear()
+        if (self.walkToggle):
+            self.__drawWalkable()
+        if (self.grassToggle):
+            self.__drawEncounters()
+
+        self.__drawSelected()
+        super().update()
+        
+
+    def __drawSelected(self):
 
         yellowPen = qtg.QPen(qtc.Qt.yellow)
         yellowPen.setWidth(1)
         
         for point in self.selected:
             self.addRect(point[0],point[1],10,10,yellowPen)
-        
+
+    def __drawWalkable(self):
+        grayPen = qtg.QPen(qtc.Qt.gray)
+        grayPen.setWidth(1)
+        height = len(self.mapData)
+        width = len(self.mapData[0])
+        for r in range(0, height):
+            for c in range(0, width):
+                if self.mapData[r][c] == 0:
+                    continue
+                else:
+                    xCoord = c * 10
+                    yCoord = r * 10
+                    self.addRect(xCoord,yCoord,10,10,grayPen)
+
+    def __drawEncounters(self):
+        redPen = qtg.QPen(qtc.Qt.red)
+        redPen.setWidth(1)
+        height = len(self.mapData)
+        width = len(self.mapData[0])
+        for r in range(0, height):
+            for c in range(0, width):
+                if self.mapData[r][c] != 2:
+                    continue
+                else:
+                    xCoord = c * 10
+                    yCoord = r * 10
+                    self.addRect(xCoord,yCoord,10,10,redPen)
+
 
 class MainWindow(qtw.QMainWindow, Ui_MainWindow):
 
@@ -46,21 +93,8 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
         self.grassButton.clicked.connect(lambda: self.grassClicked())
         self.runButton.clicked.connect(lambda: self.runClicked())
 
-        # only generate walkable data if it doesn't exist
-        try:
-            self.mapData = np.loadtxt('mapData.csv', delimiter=',')
-        except:
-            self.mapData = initWalkable()
-            np.savetxt('mapData.csv', mapData, delimiter=',')
-
-        # button pressed status
-        self.walkToggle = False
-        self.grassToggle = False
-        self.mapScene = GraphicsScene(self)
-        self.mapView.setScene(self.mapScene)
-
-    def mapClicked(self):
-        print("Clicked map at: ")
+        self.map = Map(self)
+        self.mapView.setScene(self.map)
 
     def undoClicked(self):
         print("UNDO Clicked")
@@ -69,64 +103,18 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
         print("REDO Clicked")
 
     def walkClicked(self):
-
-        if self.walkToggle:
-            self.mapScene.clear()
-        else:
-            self.__drawWalkable(self.mapScene)
-
-        if self.grassToggle:
-            self.__drawEncounters(self.mapScene)
-        
-        self.mapScene.update()
-        self.mapScene.redrawSelected()
-        self.walkToggle = not self.walkToggle
+        print("WALK Clicked")
+        self.map.walkToggle = not self.map.walkToggle
+        self.map.update()
 
     def grassClicked(self):
-
-        if self.grassToggle:
-            self.mapScene.clear()
-        
-        if self.walkToggle:
-            self.__drawWalkable(self.mapScene)
-
-        if not self.grassToggle:
-            self.__drawEncounters(self.mapScene)
-                    
-        self.mapScene.update()
-        self.mapScene.redrawSelected()
-        self.grassToggle = not self.grassToggle
+        print("GRASS Clicked")
+        self.map.grassToggle = not self.map.grassToggle
+        self.map.update()
 
     def runClicked(self):
         print("RUN Clicked")
 
-    def __drawWalkable(self, scene):
-        grayPen = qtg.QPen(qtc.Qt.gray)
-        grayPen.setWidth(1)
-        height = len(self.mapData)
-        width = len(self.mapData[0])
-        for r in range(0, height):
-            for c in range(0, width):
-                if self.mapData[r][c] == 0:
-                    continue
-                else:
-                    xCoord = c * 10
-                    yCoord = r * 10
-                    scene.addRect(xCoord,yCoord,10,10,grayPen)
-
-    def __drawEncounters(self, scene):
-        redPen = qtg.QPen(qtc.Qt.red)
-        redPen.setWidth(1)
-        height = len(self.mapData)
-        width = len(self.mapData[0])
-        for r in range(0, height):
-            for c in range(0, width):
-                if self.mapData[r][c] != 2:
-                    continue
-                else:
-                    xCoord = c * 10
-                    yCoord = r * 10
-                    scene.addRect(xCoord,yCoord,10,10,redPen)
 
 
 if __name__ == "__main__":
