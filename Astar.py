5a47a832299f2fdfb54f7797901b19bcd4238304
unknown <noah.v.olson@gmail.com>
2020-07-23 16:52:09 -0400

    Fixed Astar across narrow areas closes #2

--- Astar.py
+++ Astar.py
@@ -1,6 +1,13 @@
+# Credit for this: Ryan Collingwood
+# as found at https://gist.github.com/ryancollingwood/32446307e976a11a1185a5394d6657bc
+from warnings import warn
+import heapq
 
-class Node():
-    """A node class for A* Pathfinding"""
+
+class Node:
+    """
+    A node class for A* Pathfinding
+    """
 
     def __init__(self, parent=None, position=None):
         self.parent = parent
@@ -13,9 +20,35 @@ class Node():
     def __eq__(self, other):
         return self.position == other.position
 
+    def __repr__(self):
+        return f"{self.position} - g: {self.g} h: {self.h} f: {self.f}"
 
-def astar(maze, start, end):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+    # defining less than for purposes of heap queue
+    def __lt__(self, other):
+        return self.f < other.f
+
+    # defining greater than for purposes of heap queue
+    def __gt__(self, other):
+        return self.f > other.f
+
+
+def return_path(current_node):
+    path = []
+    current = current_node
+    while current is not None:
+        path.append(current.position)
+        current = current.parent
+    return path[::-1]  # Return reversed path
+
+
+def astar(maze, start, end, allow_diagonal_movement=True):
+    """
+    Returns a list of tuples as a path from the given start to the given end in the given maze
+    :param maze:
+    :param start:
+    :param end:
+    :return:
+    """
 
     # Create start and end node
     start_node = Node(None, start)
@@ -27,42 +60,48 @@ def astar(maze, start, end):
     open_list = []
     closed_list = []
 
-    # Add the start node
-    open_list.append(start_node)
+    # Heapify the open_list and Add the start node
+    heapq.heapify(open_list)
+    heapq.heappush(open_list, start_node)
+
+    # Adding a stop condition
+    outer_iterations = 0
+    max_iterations = (len(maze[0]) * len(maze) // 2)
+
+    # what squares do we search
+    adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
+    if allow_diagonal_movement:
+        adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1),)
 
     # Loop until you find the end
     while len(open_list) > 0:
+        outer_iterations += 1
 
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
+        if outer_iterations > max_iterations:
+            # if we hit this point return the path such as it is
+            # it will not contain the destination
+            warn("giving up on pathfinding too many iterations")
+            return return_path(current_node)
 
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
+            # Get the current node
+        current_node = heapq.heappop(open_list)
         closed_list.append(current_node)
 
         # Found the goal
         if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
+            return return_path(current_node)
 
         # Generate children
         children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+        for new_position in adjacent_squares:  # Adjacent squares
 
             # Get node position
             node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
             # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (
+                    len(maze[len(maze) - 1]) - 1) or node_position[1] < 0:
                 continue
 
             # Make sure walkable terrain
@@ -77,21 +116,23 @@ def astar(maze, start, end):
 
         # Loop through children
         for child in children:
-
             # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
+            if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
+                continue
 
             # Create the f, g, and h values
             child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
-            child.f = child.g + child.h #* maze[child.position[0]][child.position[1]]
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + (
+                        (child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
 
             # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
+            if len([open_node for open_node in open_list if
+                    child.position == open_node.position and child.g > open_node.g]) > 0:
+                continue
 
             # Add the child to the open list
-            open_list.append(child)
+            heapq.heappush(open_list, child)
+
+    warn("Couldn't get a path to destination")
+    return None
--- SaiBot5.0.py
+++ SaiBot5.0.py
@@ -16,15 +16,15 @@ class Map(qtw.QGraphicsScene):
             self.mapData = np.loadtxt('mapData.csv', delimiter=',')
         except:
             self.mapData = initWalkable()
-            np.savetxt('mapData.csv', mapData, delimiter=',')
-        
+            np.savetxt('mapData.csv', self.mapData, delimiter=',')
+
         self.__selected = deque()
         self.__allPaths = deque()
         self.__undone = deque()
         self.__undonePaths = deque()
         self.walkToggle = False
         self.grassToggle = False
-        
+
     def mousePressEvent(self, event):
 
         dataX = int(event.scenePos().x() // 10)
@@ -39,17 +39,17 @@ class Map(qtw.QGraphicsScene):
 
         self.__selected.append((x, y))
         self.__addPath()
-        self.__undone = deque() # upon making a change, undone cache is cleared
+        self.__undone = deque()  # upon making a change, undone cache is cleared
         self.__undonePaths = deque()
         self.update()
 
     def update(self):
         self.clear()
-        if (self.walkToggle):
+        if self.walkToggle:
             self.__drawWalkable()
-        if (self.grassToggle):
+        if self.grassToggle:
             self.__drawEncounters()
-            
+
         self.__drawAllPaths()
         self.__drawSelected()
 
@@ -70,36 +70,34 @@ class Map(qtw.QGraphicsScene):
         if not self.__undonePaths:
             return
         self.__allPaths.append(self.__undonePaths.pop())
-        
 
     def __drawSelected(self):
 
-        orangePen = qtg.QPen(qtg.QColor(255,165,0))
+        orangePen = qtg.QPen(qtg.QColor(255, 165, 0))
         orangePen.setWidth(2)
-        
+
         for point in self.__selected:
-            self.addRect(point[0],point[1],10,10,orangePen)
+            self.addRect(point[0], point[1], 10, 10, orangePen)
 
     def __addPath(self):
         if len(self.__selected) <= 1:
             return
 
-        # start is end of deque (most recently added)
-        start = ((self.__selected[-1][1] // 10), (self.__selected[-1][0] // 10))
-        end = ((self.__selected[-2][1] // 10), (self.__selected[-2][0] // 10))
+        # most recent tile selection will be the end of the path
+        end = ((self.__selected[-1][1] // 10), (self.__selected[-1][0] // 10))
+        start = ((self.__selected[-2][1] // 10), (self.__selected[-2][0] // 10))
 
+        print("Start: " + str(start))
+        print("End: " + str(end))
         self.__allPaths.append(astar(self.mapData, start, end))
-        #print(self.__allPaths)
 
     def __drawAllPaths(self):
-
         yellowPen = qtg.QPen(qtc.Qt.yellow)
         yellowPen.setWidth(1)
 
         for path in self.__allPaths:
             for point in path:
-                self.addRect(point[1] * 10,point[0] * 10,10,10,yellowPen)
-                
+                self.addRect(point[1] * 10, point[0] * 10, 10, 10, yellowPen)
 
     def __drawWalkable(self):
         grayPen = qtg.QPen(qtc.Qt.gray)
@@ -113,7 +111,7 @@ class Map(qtw.QGraphicsScene):
                 else:
                     xCoord = c * 10
                     yCoord = r * 10
-                    self.addRect(xCoord,yCoord,10,10,grayPen)
+                    self.addRect(xCoord, yCoord, 10, 10, grayPen)
 
     def __drawEncounters(self):
         redPen = qtg.QPen(qtc.Qt.red)
@@ -127,7 +125,7 @@ class Map(qtw.QGraphicsScene):
                 else:
                     xCoord = c * 10
                     yCoord = r * 10
-                    self.addRect(xCoord,yCoord,10,10,redPen)
+                    self.addRect(xCoord, yCoord, 10, 10, redPen)
 
 
 class MainWindow(qtw.QMainWindow, Ui_MainWindow):
@@ -166,12 +164,10 @@ class MainWindow(qtw.QMainWindow, Ui_MainWindow):
         print("RUN Clicked")
 
 
-
 if __name__ == "__main__":
-
     app = qtw.QApplication([])
 
     mainWindow = MainWindow()
     mainWindow.show()
-    
+
     app.exec_()
